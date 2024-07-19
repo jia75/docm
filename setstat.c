3850a9b04f2e7dc7a112b757d04467435d31dfbb
Dimitris Hargitt <jiahrgtt@gmail.com>
2024-07-19 10:34:21 +0300

    fix setstat and cleanup

--- setstat.c
+++ setstat.c
@@ -12,38 +12,25 @@ int gdocm_setstat(int argc, char *argv[]) {
 	getcwd(cwd, sizeof(cwd));
 	char gdocmDir[150];
     sprintf(gdocmDir, "%s/.gdocm/doc", cwd);
-
     if (mkdir(gdocmDir, 0777) != 0 && errno != EEXIST) return -1;
 
     char docLoc[200];
 	sprintf(docLoc, "%s/%s.d", gdocmDir, argv[2]);
-
     int fd = open(docLoc, O_RDWR);
-
     if (fd < 0) return -1;	
 	
 	char[512] buffer;
-	
 	int currentLength;
-	
-	if ((currentLength = read(fd, c, 512)) < 0) return -1;
-
+	if ((currentLength = read(fd, buffer, 512)) < 0) return -1;
 
     newContents[550];
-	
 	strcpy(newContents, argv[3]);
-	
 	strcat(newContents, buffer);
 
     close(fd);
 	
 	fd = creat(docLoc, 0777);
-
     if (fd < 0) return -1;
-	
 	write(fd, buffer, strlen(buffer));
-	
 	close(fd);
-
-    createDocument(1, existingFiles, StrToInt(argv[2]), newStat, existingName);
 }
