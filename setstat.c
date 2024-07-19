85a460d9f27e5ff456b03057208a82bc1fc8f932
Dimitris Hargitt <jiahrgtt@gmail.com>
2024-07-19 10:32:01 +0300

    add setstat

---
+++ setstat.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int gdocm_setstat(int argc, char *argv[]) {
+    if (argc < 4) fprintf(stderr, "see usage with gdocm help\n");
+
+    char cwd[100];
+	getcwd(cwd, sizeof(cwd));
+	char gdocmDir[150];
+    sprintf(gdocmDir, "%s/.gdocm/doc", cwd);
+
+    if (mkdir(gdocmDir, 0777) != 0 && errno != EEXIST) return -1;
+
+    char docLoc[200];
+	sprintf(docLoc, "%s/%s.d", gdocmDir, argv[2]);
+
+    int fd = open(docLoc, O_RDWR);
+
+    if (fd < 0) return -1;	
+	
+	char[512] buffer;
+	
+	int currentLength;
+	
+	if ((currentLength = read(fd, c, 512)) < 0) return -1;
+
+
+    newContents[550];
+	
+	strcpy(newContents, argv[3]);
+	
+	strcat(newContents, buffer);
+
+    close(fd);
+	
+	fd = creat(docLoc, 0777);
+
+    if (fd < 0) return -1;
+	
+	write(fd, buffer, strlen(buffer));
+	
+	close(fd);
+
+    createDocument(1, existingFiles, StrToInt(argv[2]), newStat, existingName);
+}
